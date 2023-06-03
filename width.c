68d1addfe5ea3d4ecb36c9be119a3f41b546ea06
high20212021 <high20212021@outlook.com>
2023-06-04 07:58:10 +0800

    width.c

---
+++ width.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define PAGES 959
+
+int main(void){
+  printf("*%d*\n", PAGES);
+  printf("*%2d*\n", PAGES);
+  printf("*%10d*\n", PAGES);
+  return 0;
+  printf("*%-10d*\n", PAGES);
+}
