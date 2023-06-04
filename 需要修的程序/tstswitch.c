b1dc82e3ab702158f5b44bc6a74d3e92f8aed0bf
high20212021 <high20212021@outlook.com>
2023-06-04 22:51:48 +0800

    搞练习，bug还没修完，段错误先修了awa

--- 需要修的程序/tstswitch.c
+++ 需要修的程序/tstswitch.c
@@ -4,23 +4,21 @@
 
 int main(void){
   char ch;
-  int num;
-  int num1;
-  int num2;
+  char ch2;
   int select;
   printf("请选择(敲序号):\n1.ASCII转字符\n2.字符转ASCII\n");
-  scanf("%d", select);
+  scanf("%d", &select);
   switch (select) {
     case 1:
       printf("请输入ASCII编码:\n");
-      scanf("%d", ch);
+      scanf("%d",&ch);
       int a = ch;
-      printf("此编码对应的字符是:%c", a);
+      printf("此编码对应的字符是: %c ", a);
       break;
     case 2:
       printf("请输入字符:\n");
-      scanf("%c", ch);
-      printf("此字符对应的编码是:%d", ch);
+      scanf("%c",&ch2);
+      printf(" %c 对应的编码是: %d ", ch2,ch2);
       break;
   }
   return 0;
