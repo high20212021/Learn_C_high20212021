d1522781705fd9f8a6404b24250f09f9d193abec
high20212021 <high20212021@outlook.com>
2023-06-18 19:41:16 +0800

    由autogit.sh自动提交

---
+++ test_no_reading.c
@@ -0,0 +1,19 @@
+/* 反wusheng */
+#include <stdio.h>
+#define e int
+#define ee main
+#define eee (
+#define eeee void
+#define eeeee )
+#define eeeeee {
+#define eeeeeee printf
+#define eeeeeeee }
+#define eeeeeeeee return
+#define eeeeeeeeee 0
+#define eeeeeeeeeee ;
+#define eeeeeeeeeeee "你是傻逼"
+
+e ee eee eeee eeeee eeeeee
+  eeeeeee eee eeeeeeeeeeee eeeee eeeeeeeeeee
+  eeeeeeeee eeeeeeeeee eeeeeeeeeee
+eeeeeeee
